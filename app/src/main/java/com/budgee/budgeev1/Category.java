2e242cbb97c5a17ce5b63a07399c4c83bcac4f42
Twilgrimm <grimmvangr@gmail.com>
2017-02-20 21:54:58 +0000

    Hope this works.

---
+++ app/src/main/java/com/budgee/budgeev1/Category.java
@@ -0,0 +1,26 @@
+package com.budgee.budgeev1;
+
+/**
+ * Created by Will on 20/02/2017.
+ */
+
+public class Category {
+    private int categoryID;
+    private String categoryName;
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
